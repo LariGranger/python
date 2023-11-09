4249c8001a1bc817c4c61bd9ed1453b648031054
Larissa Pinheiro <vpinheiro.larissa@gmail.com>
2023-11-09 11:34:21 -0300

    Jogo de adivinhação - completo

---
+++ jogos/forca.py
@@ -0,0 +1,9 @@
+def jogo():
+    print("*********************************")
+    print("Bem vindo ao jogo de Forca!")
+    print("*********************************")
+
+    print("Fim do jogo")
+
+if(__name__ == "__main__"):
+    jogo()
